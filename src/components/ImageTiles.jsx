76b99d1dc42f6ea730c71632d6e919f9b541ffb9
Edwin Park <esp1@cornell.edu>
2023-01-23 17:51:22 -0800

    improving mobile rendering

--- src/components/ImageTiles.jsx
+++ src/components/ImageTiles.jsx
@@ -29,11 +29,11 @@ export default function ImageTiles(props) {
       </div>
 
       <div class={`${isModalOpen() ? "block" : "hidden"} fixed top-0 left-0 w-screen h-screen`}>
-        <div class="w-screen h-screen backdrop-blur-md flex">
+        <div class="w-screen h-screen backdrop-blur-md flex justify-center">
           {/* previous image */}
           {props.images.length <= 1
             ? <div class="grow" />
-            : <button class="grow grid place-content-center stroke-transparent hover:stroke-white hover:stroke-[8px]" type="button" onClick={() => incModalImageIdx(-1)}>
+            : <button class="hidden lg:block lg:grow lg:grid place-content-center stroke-white stroke-[4px]" type="button" onClick={() => incModalImageIdx(-1)}>
               <svg stroke-linecap="round" width="100" height="100">
                 <line x1="70" y1="10" x2="30" y2="50" />
                 <line x1="30" y1="50" x2="70" y2="90" />
@@ -44,7 +44,7 @@ export default function ImageTiles(props) {
           {/* image */}
           {isModalOpen() &&
             <img
-              class="m-8"
+              class="m-8 object-contain"
               src={getModalImage()}
             />
           }
@@ -52,7 +52,7 @@ export default function ImageTiles(props) {
           {/* next image */}
           {props.images.length <= 1
             ? <div class="grow" />
-            : <button class="grow grid place-content-center stroke-transparent hover:stroke-white hover:stroke-[8px]" type="button" onClick={() => incModalImageIdx(1)}>
+            : <button class="hidden lg:block lg:grow lg:grid place-content-center stroke-white stroke-[4px]" type="button" onClick={() => incModalImageIdx(1)}>
               <svg stroke-linecap="round" width="100" height="100">
                 <line x1="30" y1="10" x2="70" y2="50" />
                 <line x1="70" y1="50" x2="30" y2="90" />
@@ -68,7 +68,7 @@ export default function ImageTiles(props) {
           onClick={closeModal}
         >
           <svg width="100" height="100">
-            <g class="stroke-white stroke-[8px]" stroke-linecap="round">
+            <g class="stroke-white stroke-[4px]" stroke-linecap="round">
               <line x1="30" y1="30" x2="70" y2="70" />
               <line x1="30" y1="70" x2="70" y2="30" />
             </g>
